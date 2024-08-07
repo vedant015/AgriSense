ec115e3d89334729fab5c44eb63d749bc4a50101
vedant015 <121847073+vedant015@users.noreply.github.com>
2024-08-07 06:50:51 +0530

    demo.py

--- demo.py
+++ demo.py
@@ -1,12 +1,30 @@
 import pyfirmata
 import time
-pin=13
+
 port="COM9"
 board=pyfirmata.Arduino(port)
+soil=board.get_pin('a:0:i')
+pump=board.get_pin('d:7:o')
+moisture_treshold=500;
+
+it=pyfirmata.util.Iterator(board)
+it.start()
+
+# Wait for the iterator to start
+time.sleep(1)
+
+def pump_on():
+    pump.write(1)
+
+def pump_off():
+    pump.write(0)
 while True:
-    board.digital[pin].write(1)
-    time.sleep(5)
-    print("on")
-    board.digital[pin].write(0)
-    time.sleep(5)
-    print("off")
+    ar=soil.read()
+    x=(1-ar)*1000
+    print(x)
+    if x<moisture_treshold:
+        pump_on()
+    else:
+        pump_off()
+    time.sleep(10)
+
